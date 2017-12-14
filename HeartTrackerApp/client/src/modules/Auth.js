4dd43deda814e1f92813394ff3f794d423bf73ec
Steven Ciasullo <sjciasullo@gmail.com>
2017-12-14 14:49:53 -0500

    comment auth helper

--- HeartTrackerApp/client/src/modules/Auth.js
+++ HeartTrackerApp/client/src/modules/Auth.js
@@ -1,17 +1,21 @@
 // helpers for auth
 class Auth {
+  // set the given token to storage
   static authenticateToken(token) {
     sessionStorage.setItem('token', token);
   }
 
+  // checks if there is a token in storage
   static isUserAuthenticated() {
     return sessionStorage.getItem('token') !== null;
   }
 
+  // logout
   static deauthenticateUser() {
     sessionStorage.removeItem('token');
   }
 
+  // get the token from storage
   static getToken() {
     return sessionStorage.getItem('token');
   }
