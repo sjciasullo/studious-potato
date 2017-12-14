b29cf9244e2d2b1a3a78e26cdf6fd5b4c43ee832
Steven Ciasullo <sjciasullo@gmail.com>
2017-12-14 18:46:25 -0500

    register form

--- HeartTrackerApp/client/src/components/RegisterForm.jsx
+++ HeartTrackerApp/client/src/components/RegisterForm.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const RegisterForm = function(props) {
   // prop list
   const toggleForm = props.toggleForm;
-  const submitLogin = props.submitLogin;
+  const submitRegister = props.submitRegister;
   const handleChange = props.handleChange;
   const username = props.username;
   const password = props.password;
@@ -13,6 +13,17 @@ const RegisterForm = function(props) {
   return(
     <div>
       <h2>Register new user</h2>
+      <form onSubmit={submitRegister}>
+        <input name="name" type='text' onChange={handleChange} 
+            placeholder="Name" value={name}/>
+        <input name="username" type='text' onChange={handleChange} 
+          placeholder="Username" value={username}/>
+        <input name="password" type='password' onChange={handleChange} 
+          placeholder="Password" value={password}/>
+        <input name="email" type='text' onChange={handleChange} 
+          placeholder="Email" value={email}/>
+        <input type='submit' value="Register"/>
+      </form>
       <p>Already have an account? <span className='changeform' onClick={toggleForm}>Login</span></p>
     </div>
   )
