e160e92d4cafc7f440a99867422f5c57ca6d1532
Steven Ciasullo <sjciasullo@gmail.com>
2017-12-14 11:34:32 -0500

    begin User model

--- HeartTrackerApp/app/controllers/application_controller.rb
+++ HeartTrackerApp/app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::API
+  # give access to token creation
+  include ActionController::HttpAuthentication::Token::ControllerMethods
 end
--- HeartTrackerApp/app/models/user.rb
+++ HeartTrackerApp/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  # ensures uniequeness of username
+  validates_uniqueness_of :username
+  #handles password hashing
+  has_secure_password
 end
