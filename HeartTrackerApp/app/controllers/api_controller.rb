cdb223617164ea9725a3e7720739ec26ce34e500
Steven Ciasullo <sjciasullo@gmail.com>
2017-12-14 11:57:05 -0500

    implement ApiController and SessionsController < ApiController

---
+++ HeartTrackerApp/app/controllers/api_controller.rb
@@ -0,0 +1,23 @@
+class ApiController < ApplicationController
+  def require_login
+    authenticate_token || render_unauthorized("Access denied")
+  end
+
+  def current_user
+    @current_user ||= authenticate_token
+  end
+
+  protected
+  # send an error message if client performs unauthorized action
+  def render_unauthorized(message)
+    errors = {errors: [ detail: message ] }
+    render json: errors, status: :unauthorized
+  end
+
+  private
+  def authenticate_token
+    authenticate_with_http_token do | token, options |
+      User.find_by(auth_token: token)
+    end
+  end
+end
---
+++ HeartTrackerApp/app/controllers/sessions_controller.rb
@@ -0,0 +1,31 @@
+class SessionsController < ApiController
+  skip_before_action :require_login, only: [:create], raise: false
+
+  def create
+    # if the user login is valid, make a new token for user, then send the token to front
+    if user = User.validate_login(params[:username], params[:password])
+      allow_token_to_be_used_only_once_for(user)
+      send_token_for_valid_login_of(user)
+    else
+      # uses api_controller method to render an error message
+      render_unauthorized("Error with your login or password")
+    end
+  end
+
+  def destroy
+    logout
+    head: ok
+  end
+
+  private
+
+  def send_token_for_valid_login_of(user)
+    render json: {token: user.auth_token}
+  end
+
+  def logout
+    #uses api controller to take token off of user
+    current_user.invalidate_token
+  end
+
+end
