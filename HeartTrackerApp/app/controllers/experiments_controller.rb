45b0446ac398106bb5799ccaeeb741e6ee86de35
Steven Ciasullo <sjciasullo@gmail.com>
2017-12-15 17:11:49 -0500

    start experiments_controller

--- HeartTrackerApp/app/controllers/experiments_controller.rb
+++ HeartTrackerApp/app/controllers/experiments_controller.rb
@@ -1,2 +1,16 @@
-class ExperimentsController < ApplicationController
+class ExperimentsController < ApiController
+  def index
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
