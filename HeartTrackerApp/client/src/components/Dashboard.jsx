a4910a2610aa45212017913332be7c3120ebfda7
Steven Ciasullo <sjciasullo@gmail.com>
2017-12-16 13:33:31 -0500

    conduct fetch from client to get experiments for logged in user

--- HeartTrackerApp/client/src/components/Dashboard.jsx
+++ HeartTrackerApp/client/src/components/Dashboard.jsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Auth from '../modules/Auth';
 
-const Dashboard = function(props) {
-  return(
-    <h2>Welcome user!</h2>
-  )
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      apiLoaded: false,
+      experiments: []
+    }
+    this.getUserExperiments = this.getUserExperiments.bind(this);
+  }
+
+  getUserExperiments(){
+    fetch('/experiments', {
+      method: 'GET',
+      headers: {
+        'Authorization': `Token ${Auth.getToken()}`,
+        token: Auth.getToken(),
+      }
+    }).then( res => res.json())
+    .then( json => {
+      this.setState({
+        apiLoaded: true,
+        experiments: json.experiments,
+      })
+    }).catch(err => {
+      console.log(err);
+    })
+  }
+
+  componentDidMount(){
+    this.getUserExperiments();
+  }
+
+  render() {
+    return(
+      <div className='dash_container'>
+        <h2>Your Experiments</h2>
+        { this.state.apiLoaded ? (
+          <p>experiments</p>
+        ) : (
+          <p>Loading</p>
+        ) }
+      </div>
+    )
+  }
 }
 
 export default Dashboard;
