c768d3526369d5e6c3f9cfc47bfa56aa1a0a7a2e
Steven Ciasullo <sjciasullo@gmail.com>
2017-12-19 16:23:38 -0500

    logoooooooo

--- HeartTrackerApp/client/src/components/Header.jsx
+++ HeartTrackerApp/client/src/components/Header.jsx
@@ -4,7 +4,8 @@ const Header = function(props){
   const logout = props.logout; // logout method
   return(
     <header>
-      <div className='logo'><a href='/'>logo</a></div>
+      <div className='logo'><a href='/'><i className="fa fa-heartbeat fa-3x"></i></a></div>
+      
       <div className='user-nav'>
         <div>
           <a href='/dashboard'>username</a>
