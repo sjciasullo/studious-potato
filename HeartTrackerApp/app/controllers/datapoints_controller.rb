3e0862903e4600a27556ee6c78ca0312923560b4
Steven Ciasullo <sjciasullo@gmail.com>
2017-12-18 19:10:43 -0500

    datapoints_controller

---
+++ HeartTrackerApp/app/controllers/datapoints_controller.rb
@@ -0,0 +1,48 @@
+class DatapointsController < ApiController
+  before_action :require_login
+
+  def index
+    render json: {
+      data: Datapoint.all.where(trial_id: params[:trial_data])
+    }
+  end
+
+  def create
+    datapoint = Datapoint.new(datapoint_params)
+    datapoint.trial_id = params[:trial_id]
+    if datapoint.save
+      render json: {
+        message: 'ok',
+        datapoint: datapoint,
+      }
+    else
+      render json: {
+        message: 'could not create datapoint'
+      }
+    end
+  end
+
+  def update
+    datapoint = Datapoint.find(params[:id])
+    if datapoint.update(trial_params)
+      render json: {
+        message: 'ok',
+        datapoint: datapoint,
+      }
+    else
+      render json: {
+        message: 'Failed. Could not edit datapoint'
+      }
+    end
+  end
+
+  def destroy
+    Datapoint.delete(params[:id])
+    render json: {message 'ok'}
+  end
+
+  private
+  def datapoint_params
+    params.require(:datapoint).permit(:heartrate)
+  end
+end
---
+++ HeartTrackerApp/test/controllers/datapoints_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class DatapointsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
