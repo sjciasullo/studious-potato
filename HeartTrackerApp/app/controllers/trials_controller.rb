4672dd65d2ff862feb2a50b275a8ba4cccd184e7
Steven Ciasullo <sjciasullo@gmail.com>
2017-12-19 14:26:38 -0500

    format dates in trials controller

--- HeartTrackerApp/app/controllers/trials_controller.rb
+++ HeartTrackerApp/app/controllers/trials_controller.rb
@@ -2,15 +2,36 @@ class TrialsController < ApiController
   before_action :require_login
 
   def index
+    trials = Trial.all.where(experiment_id: params[:experiment_id])
+    formattedTrials = []
+      trials.each do |trial|
+        formatted = {
+          id: trial.id,
+          trial_num: trial.trial_num,
+          notes: trial.notes,
+          experiment_id: trial.experiment_id,
+          created_at: trial.created_at.to_s(:long),
+          updated_at: trial.updated_at.to_s(:long),
+        }
+        formattedTrials.push(formatted)
+      end
     render json: {
-      trials: Trial.all.where(experiment_id: params[:experiment_id])
+      trials: formattedTrials
     }
   end
 
   def show
     trial = Trial.find(params[:id])
+    formatted = {
+      id: trial.id,
+      trial_num: trial.trial_num,
+      notes: trial.notes,
+      experiment_id: trial.experiment_id,
+      created_at: trial.created_at.to_s(:long),
+      updated_at: trial.updated_at.to_s(:long),
+    }
     render json: {
-      trial: trial,
+      trial: formatted,
       data: trial.datapoints
     }
   end
