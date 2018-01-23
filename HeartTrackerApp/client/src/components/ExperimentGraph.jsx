d6e9a715ddfc00de70d9cea68f861833cc9e38f1
Steven Ciasullo <sjciasullo@gmail.com>
2018-01-23 01:10:23 -0700

    make graph component

---
+++ HeartTrackerApp/client/src/components/ExperimentGraph.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import * as d3 from 'd3';
+
+class ExperimentGraph extends Component {
+  render() {
+    <div>
+      This is a Graph
+    </div>
+  }
+}
