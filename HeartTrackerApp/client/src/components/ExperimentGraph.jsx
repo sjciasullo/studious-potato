b50d2aad6bd8c42c71a8364d4ba85a17122d1a48
Steven Ciasullo <sjciasullo@gmail.com>
2018-01-23 13:13:17 -0700

    add sources

--- HeartTrackerApp/client/src/components/ExperimentGraph.jsx
+++ HeartTrackerApp/client/src/components/ExperimentGraph.jsx
@@ -1,10 +1,14 @@
+// Sources:
+// -- https://hackernoon.com/building-d3-components-with-react-7510e4743288
+// -- https://bl.ocks.org/mbostock/3884955
+
 import React, { Component } from 'react';
 import * as d3 from 'd3';
 
 class ExperimentGraph extends Component {
   render() {
-    <div>
-      This is a Graph
-    </div>
+    return(
+      <div ref='graph'></div>
+    )
   }
 }
