0ba0c6c2a25cad831d4853b5f342f734d4608de7
Steven Ciasullo <sjciasullo@gmail.com>
2018-01-24 09:50:11 -0700

    set context for svg file

--- HeartTrackerApp/client/src/components/ExperimentGraph.jsx
+++ HeartTrackerApp/client/src/components/ExperimentGraph.jsx
@@ -6,6 +6,23 @@ import React, { Component } from 'react';
 import * as d3 from 'd3';
 
 class ExperimentGraph extends Component {
+
+  //when component mounts, set the context of an svg file that d3 will create
+  componentDidMount() {
+    this.setContext();
+  }
+
+  // called when component mounts to give d3 a canvas (svg file) to draw on
+  // -- sets attributes for the svg file
+  setContext() {
+    return d3.select(this.refs.graph).append('svg')
+      .attr('height', '300px')
+      .attr('width', '500px')
+      .attr('id', 'graph')
+      .append('g') // are we appending a graph on the svg canvas after setting the svg attrs?
+      .attr('transform', `translate(150, 150)`); // what does this transform do?
+  }
+
   render() {
     return(
       <div ref='graph' className='experiment-graph'></div>
