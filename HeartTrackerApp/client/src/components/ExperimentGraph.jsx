00cb3b3428e05772bec54f69e52e64bcace31fee
Steven Ciasullo <sjciasullo@gmail.com>
2018-01-25 11:46:20 -0500

    this tutorial was old d3 version so trying a new method

--- HeartTrackerApp/client/src/components/ExperimentGraph.jsx
+++ HeartTrackerApp/client/src/components/ExperimentGraph.jsx
@@ -1,6 +1,7 @@
 // Sources:
 // -- https://hackernoon.com/building-d3-components-with-react-7510e4743288
 // -- https://bl.ocks.org/mbostock/3884955
+// -- https://bl.ocks.org/pstuffa/26363646c478b2028d36e7274cedefa6
 
 import React, { Component } from 'react';
 import * as d3 from 'd3';
@@ -8,22 +9,22 @@ import * as d3 from 'd3';
 class ExperimentGraph extends Component {
   //test data
   data = [{
-    "datapoint number": "0",
+    "datapoint number": "1",
     "heartrate": "80"
   }, {
-    "datapoint number": "1",
+    "datapoint number": "2",
     "heartrate": "85"
   }, {
-    "datapoint number": "2",
+    "datapoint number": "3",
     "heartrate": "90"
   }, {
-    "datapoint number": "3",
+    "datapoint number": "4",
     "heartrate": "100"
   }, {
-    "datapoint number": "4",
+    "datapoint number": "5",
     "heartrate": "50"
   }, {
-    "datapoint number": "5",
+    "datapoint number": "6",
     "heartrate": "80"
   }]
 
@@ -39,13 +40,33 @@ class ExperimentGraph extends Component {
       .attr('height', '300px')
       .attr('width', '500px')
       .attr('id', 'graph')
-      /*
       .append('g') // are we appending a graph on the svg canvas after setting the svg attrs?
-      .attr('transform', `translate(150, 150)`); // what does this transform do?
-      */
+      .attr('transform', `translate(10, 20)`); // what does this transform do?
   }
 
   drawAxes(context) {
+    // source: 
+    // https://code.tutsplus.com/tutorials/building-a-multi-line-chart-using-d3js--cms-22935
+    
+    //constants for scaling
+    const WIDTH = 500;
+    const HEIGHT = 300;
+    const MARGINS = {
+      top: 10,
+      right: 10,
+      bottom: 10,
+      left: 30
+    }
+
+    //sets the scale of drawable range (x and y axis)
+    // -- domains are hard coded will need to be adjusted
+    const xScale = d3.scaleLinear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([0,7])
+    const yScale = d3.scaleLinear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([40,110])
+    const xAxis = d3.svg.axis().scale(xScale);
+    const yAxis = d3.svg.axis().scale(yScale);
+
+    context.append("svg:g").call(xAxis);
+
     return context.append('path')
       .datum({ })
   }
